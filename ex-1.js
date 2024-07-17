be2e2e5db1b8cb13e650e78053e1a3e82d957030
Paktakraw <tresarpnat27@gmail.com>
2024-07-17 14:50:36 +0700

    Algorithm: Problem Solving 2 complete

--- ex-1.js
+++ ex-1.js
@@ -1,5 +1,44 @@
 function findStudentIndex(students, searchStudent) {
-	// เริ่มเขียนโค้ดตรงนี้จ้า
+  // เริ่มเขียนโค้ดตรงนี้จ้า
+  let left = 0;
+  let right = students.length - 1;
+
+  while (left <= right) {
+    const mid = Math.floor((left + right) / 2);
+
+    if (students[mid] === searchStudent) {
+      return mid;
+    } else if (students[mid] < searchStudent) {
+      left = mid + 1;
+    } else {
+      right = mid - 1;
+    }
+  }
+  return -1;
 }
 
+console.log(
+  findStudentIndex(
+    [
+      "Andrew",
+      "Bobby",
+      "Diana",
+      "George",
+      "Hannah",
+      "Isaac",
+      "Jasmine",
+      "John",
+    ],
+    "John"
+  )
+);
+
+console.log(
+  findStudentIndex(
+    ["Alice", "Bob", "Charlie", "Diana", "Ethan", "Fiona"],
+    "Andrew"
+  )
+);
+
 // ตอบคำถามตรงนี้จ้า
+// Binary search มี Big O คือ O(log n) เพราะ มีการแบ่งครึ่งขอบเขตของการค้นหา ทำให้จำนวนรอบในการทำงานเพิ่มขึ้นตามจำนวน input  ในรูปแบบ  logarithum
